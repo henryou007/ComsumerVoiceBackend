4683fd91538c356f83f389c8c229f250bc1eaf63
Henry You <henryou007@hotmail.com>
2012-03-21 13:49:27 -0400

    create CVMasterServlet and DBHelper

---
+++ src/edu/upenn/cis350/CVMasterServlet.java
@@ -0,0 +1,55 @@
+package edu.upenn.cis350;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.upenn.cis350.util.DBHelper;
+
+
+public class CVMasterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+ 
+    public CVMasterServlet() {
+        
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		DBHelper.setUpDatabase();
+		
+		
+		
+		
+		String requestType = request.getParameter("type");
+		
+		boolean succeeded =  false;
+		if (requestType!=null && requestType.length()>0){
+			//TODO: read the request
+			
+			//TODO: process the request
+			
+		}
+		
+		
+	
+		
+		DBHelper.closeDatabase();
+		out.close();
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		
+	}
+
+}
---
+++ src/edu/upenn/cis350/util/DBHelper.java
@@ -0,0 +1,17 @@
+package edu.upenn.cis350.util;
+
+public class DBHelper {
+
+	public static void setUpDatabase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void closeDatabase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
